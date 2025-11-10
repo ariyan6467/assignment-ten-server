9c4d77d7ec3cc58c1b0a8fa231f0712687e965e8
nahian jawad <nahianjawad4321@gmail.com>
2025-11-11 03:39:46 +0600

    major setup is over

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ async function run() {
 
     const db = client.db('assign_ten');
     const allAiCollection = db.collection("allAiCollection")
+     const purchaseColl = db.collection("purchaseColl");
 
     
   //post all ai data
@@ -67,6 +68,14 @@ async function run() {
   })
 
   
+//all purchase collection
+ app.get("/allpurchase",async(req,res)=>{
+    const cursor = purchaseColl.find();
+    const result = await cursor.toArray();
+    res.send(result);
+  })
+
+
   //get certai data for details
   app.get("/models/:id", async(req,res) =>{
       const id = req.params.id;
@@ -76,6 +85,68 @@ async function run() {
         res.send(result);
   })
 
+
+  //get my modals through email
+   app.get("/model/:email", async(req,res) =>{
+      const email = req.params.email;
+        const filter = {createdBy: email}
+
+        const result = await allAiCollection.find(filter).toArray();
+        res.send(result);
+  })
+
+
+  // DELETE a specific model by its ID
+    app.delete('/models/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+
+      // Try to delete the document
+      const result = await allAiCollection.deleteOne(filter);
+
+      if (result.deletedCount === 1) {
+        res.status(200).send({ message: 'Item deleted successfully' });
+      } else {
+        res.status(404).send({ message: 'Item not found' });
+      }
+    });
+
+
+ 
+  // Post purchase data
+app.post("/purchase/:id", async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+
+  try {
+  
+    const purchasedModel = await allAiCollection.findOne(filter);
+    if (!purchasedModel) {
+      return res.status(404).send({ message: 'Model not found' });
+    }
+
+   
+    const updatedModel = {
+      ...purchasedModel,
+      purchased: purchasedModel.purchased + 1 
+    };
+
+    await purchaseColl.insertOne(updatedModel); 
+
+   
+    await allAiCollection.updateOne(
+      filter,
+      { $inc: { purchased: 1 } } 
+    );
+
+    
+    res.send({ message: 'Purchase successful', model: updatedModel });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: 'Something went wrong' });
+  }
+});
 
 
     await client.db("admin").command({ ping: 1 });
